d660ec7e34f6f7a296314007f98556bf677de6bf
Victor Zimnikov <victor.zimnikov@yarndex.ru>
2026-09-22 10:50:19 +0200

    feat(domain): add MaxProjectNameLength validation

--- internal/domain/project.go
+++ internal/domain/project.go
@@ -4,9 +4,13 @@ import (
 	"errors"
 	"strings"
 	"time"
+	"unicode/utf8"
 )
 
+const MaxProjectNameLength = 100
+
 var ErrProjectNameRequired = errors.New("project name is required")
+var ErrProjectNameTooLong = errors.New("project name is too long")
 
 type ProjectID int64
 
@@ -20,8 +24,9 @@ type Project struct {
 
 func NewProject(name string, description string, now time.Time) (*Project, error) {
 	name = strings.TrimSpace(name)
-	if name == "" {
-		return nil, ErrProjectNameRequired
+
+	if err := validateName(name); err != nil {
+		return nil, err
 	}
 
 	return &Project{
@@ -34,8 +39,9 @@ func NewProject(name string, description string, now time.Time) (*Project, error
 
 func (p *Project) ChangeName(name string, now time.Time) error {
 	name = strings.TrimSpace(name)
-	if name == "" {
-		return ErrProjectNameRequired
+
+	if err := validateName(name); err != nil {
+		return err
 	}
 
 	p.Name = name
@@ -47,6 +53,18 @@ func (p *Project) ChangeName(name string, now time.Time) error {
 func (p *Project) ChangeDescription(description string, now time.Time) error {
 	p.Description = description
 	p.UpdatedAt = now
+
+	return nil
+}
+
+func validateName(name string) error {
+	if name == "" {
+		return ErrProjectNameRequired
+	}
+
+	if utf8.RuneCountInString(name) > MaxProjectNameLength {
+		return ErrProjectNameTooLong
+	}
 
 	return nil
 }
